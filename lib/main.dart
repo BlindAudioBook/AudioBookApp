e4c9b516eb92e8d29f98d83ede37509763040820
blackmoney069 <2019ucs0093@iitjammu.ac.in>
2023-06-20 18:36:15 +0530

    upload functionality added to the app

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:blind_spot/HomePage/homePage.dart';
 import 'package:blind_spot/LibraryPage/libraryPage.dart';
 import 'package:blind_spot/NowPlayingPage/nowPlayingPage.dart';
+import 'package:blind_spot/uploadPage/uploadPage.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -21,50 +22,81 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   int currentIndex = 0;
-  List<Widget> _screen = [HomePage(),NowPlayingPage(), LibraryPage()];
+  List<Widget> _screen = [HomePage(), NowPlayingPage(), LibraryPage()];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: (currentIndex!=1)?AppBar(
-        title: Text(
-          "Hi User!",
-          style: GoogleFonts.lato(
-              textStyle: TextStyle(
-                  color: Colors.black,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 30)),
-        ),
-        elevation: 0,
-        backgroundColor: Colors.white,
-        actions: <Widget>[
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: IconButton(onPressed: () {}, icon: Icon(Icons.settings, color: Colors.black,)),
-          )
-        ],
-      ):null,
+      appBar: (currentIndex != 1)
+          ? AppBar(
+              title: Text(
+                "Hi User!",
+                style: GoogleFonts.lato(
+                    textStyle: TextStyle(
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 30)),
+              ),
+              elevation: 0,
+              backgroundColor: Colors.white,
+              actions: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: IconButton(
+                      onPressed: () {},
+                      icon: Icon(
+                        Icons.settings,
+                        color: Colors.black,
+                      )),
+                )
+              ],
+            )
+          : null,
       body: _screen[currentIndex],
+      // body: uploadPage(),
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: currentIndex,
-        onTap: (index){
+        onTap: (index) {
           setState(() {
             currentIndex = index;
           });
         },
-        backgroundColor: currentIndex==1?Colors.blue:Colors.white,
-        selectedItemColor:currentIndex==1?Colors.white:Colors.blue,
-        elevation: currentIndex==1?0:30,
+        backgroundColor: currentIndex == 1 ? Colors.blue : Colors.white,
+        selectedItemColor: currentIndex == 1 ? Colors.white : Colors.blue,
+        elevation: currentIndex == 1 ? 0 : 30,
         items: [
-          BottomNavigationBarItem(icon: Icon(Icons.explore, size: 25,), label: "Explore", ),
-          BottomNavigationBarItem(icon: Icon(Icons.music_note, size: 40,), label: "Now playing"),
-          BottomNavigationBarItem(icon: Icon(Icons.book_online, size: 25,), label: "Library")
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.explore,
+              size: 25,
+            ),
+            label: "Explore",
+          ),
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.music_note,
+                size: 40,
+              ),
+              label: "Now playing"),
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.book_online,
+                size: 25,
+              ),
+              label: "Library")
         ],
       ),
-      floatingActionButton: currentIndex!=1?FloatingActionButton(
-        onPressed: (){},
-        child: Icon(Icons.add),
-        tooltip: "Upload Content",
-      ):null,
+      floatingActionButton: currentIndex != 1
+          ? FloatingActionButton(
+              onPressed: () {
+                Navigator.of(context).push(
+                    new MaterialPageRoute(builder: (BuildContext context) {
+                      return uploadPage();
+                    }));
+              },
+              child: Icon(Icons.add),
+              tooltip: "Upload Content",
+            )
+          : null,
     );
   }
 }
